dba812d49281f2ac8fa1e47ce8bc963d6091194f
Atefeh Zareh <atefe.zare@gmail.com>
2021-04-11 13:00:22 +0430

    Update willitst.java
    changing "cout" syntax to "System.out.print", the class name to "Main" (SPOJ rule), other java conventions

--- java/willitst.java
+++ java/willitst.java
@@ -1,25 +1,26 @@
-import java.io.*;
-public class willitst
-{
-  public static void main(String args[])throws IOException 
-        {
-           BufferedReader br=new BufferedReader(new InputStreamReader(system.in));
-	   long n;
-	   n=Long.parseLong(br.readLine());
-	   if(n==1)
-	   cout<<"TAK";
-	   else
-	   while(n%2==0&&n>0)
-	   {
-	         n/=2;
-		 if(n==1)
-		{
-                   System.out.println("TAK");
-		   break;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.spoj.com/problems/WILLITST/
+ */
+public class WillList {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n;
+        n = Long.parseLong(br.readLine());
+        if (n == 1)
+            System.out.println("TAK");
+        else
+            while (n % 2 == 0 && n > 0) {
+                n /= 2;
+                if (n == 1) {
+                    System.out.println("TAK");
+                    break;
                 }
-	    }
-            if(n>1)
-               System.out.println("NIE");
-	
-          }	
+            }
+        if (n > 1)
+            System.out.println("NIE");
+    }
 }
